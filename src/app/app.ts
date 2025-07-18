74fb73083104fb9205cc9c17c114c1d41c51a194
Pewep <philip.d.dumoran@gmail.com>
2025-07-19 02:47:46 +0800

    Add Toast

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected title = 'whois-lookup-frontend';
+  globalError: string | null = null;
+
+  showError(message: string) {
+    this.globalError = message;
+    setTimeout(() => {
+      this.globalError = null;
+    }, 3000);
+  }
 }
